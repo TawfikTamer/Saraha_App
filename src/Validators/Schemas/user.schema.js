b4934cb7502a66f0808b3a67cfa3fc5497ae5ee2
Tawfik Tamer <tawfiktamer.dev@gmail.com>
2025-09-08 21:54:06 +0300

    refactor(Validation): add general validation schema

--- src/Validators/Schemas/user.schema.js
+++ src/Validators/Schemas/user.schema.js
@@ -1,17 +1,21 @@
 import Joi from "joi";
+import { GeneralRules } from "./index.js";
+import { messagePrivicyEnum, genderEnum } from "../../Common/Enums/index.js";
 
 export const profileDataSchema = {
   params: Joi.object({
-    id: Joi.string().hex().length(24),
+    id: GeneralRules.objectId,
   }),
 };
 
 export const messgaeStatusSchema = {
   body: Joi.object({
-    messageState: Joi.string().valid("private", "public").required(),
+    messageState: Joi.string()
+      .valid(...Object.values(messagePrivicyEnum))
+      .required(),
   }),
   params: Joi.object({
-    messageId: Joi.string().hex().length(24),
+    messageId: GeneralRules.objectId,
   }),
 };
 
@@ -19,7 +23,7 @@ export const updateSchema = {
   body: Joi.object({
     firstName: Joi.string().min(3).max(10),
     lastName: Joi.string().min(3).max(10),
-    email: Joi.string().email(),
-    gender: Joi.string().valid("male", "female"),
+    email: GeneralRules.email,
+    gender: Joi.string().valid(...Object.values(genderEnum)),
   }),
 };
